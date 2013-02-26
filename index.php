04bb834b7ec727623437be08e9d9be371ae23ebb
Jochen Greif <jochen@opoloo.de>
2013-02-26 14:30:53 +0100

    Added Piwik tracking code

--- index.php
+++ index.php
@@ -360,6 +360,17 @@ $(document).ready(function(){
 <div id="totop">
 	<a href="#start">Zum Seitenanfang</a>
 </div>
-
+<!-- Piwik --> 
+<script type="text/javascript">
+var pkBaseURL = "http://analytics.opoloo.de/"
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 6);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://analytics.opoloo.de/piwik.php?idsite=6" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Code -->
 </body>
 </html>
