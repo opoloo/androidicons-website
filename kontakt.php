1f8eb6486beb81bb90f510816e1fccccb05acfd1
Jochen Greif <jochen@opoloo.de>
2013-05-07 16:58:40 +0200

    debugged iframes and contact page

--- kontakt.php
+++ kontakt.php
@@ -23,8 +23,7 @@
                 </p><br/>
                 <p>Wir freuen uns auf Sie.</p>
             </div>
-            <div class="col span4">
-                <a name="contact" id="contact"></a>
+            <div class="col span4" id="contact">
                 <h2>Schreiben Sie uns</h2>
                   <?php
 				    if(($send=="1") && (isset($err_text))) {
@@ -78,8 +77,6 @@
                 <span class="label">Fon</span> 06781 36009-0<br/>
                 <span class="label">Fax</span> 06781 36009-20 <br/>
                 <span class="label">Email</span> <script type="text/javascript">eval(decodeURIComponent('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%5c%22%6d%61%69%6c%74%6f%3a%69%6e%66%6f%40%73%63%68%77%69%6d%6d%62%61%64%2d%77%65%6e%64%65%6c%2e%64%65%5c%22%3e%69%6e%66%6f%40%73%63%68%77%69%6d%6d%62%61%64%2d%77%65%6e%64%65%6c%2e%64%65%3c%5c%2f%61%3e%27%29%3b'))</script>
-
-                </p>
             </div>
         </div>
     </div>
@@ -91,14 +88,14 @@
                 <h1>// So finden Sie uns</h1>
             </div>
         </div>
-        <div class="row">
+        <div class="row maps">
             <div class="col span6">
                 <h2>Anfahrt Idar-Oberstein</h2>
-                <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.de/maps?f=q&amp;source=s_q&amp;hl=de&amp;geocode=&amp;q=Wendel+Schwimmbadbau,+Wilhelmstra%C3%9Fe,+Idar-Oberstein&amp;aq=0&amp;oq=wendel+schwimm&amp;sll=51.175806,10.454119&amp;sspn=9.289357,20.808105&amp;t=h&amp;ie=UTF8&amp;hq=Wendel+Schwimmbadbau,&amp;hnear=Wilhelmstra%C3%9Fe,+Oberstein+55743+Idar-Oberstein,+Rheinland-Pfalz&amp;ll=49.702513,7.335062&amp;spn=0.018707,0.040641&amp;z=14&amp;iwloc=A&amp;cid=27701883225859051&amp;output=embed"></iframe><br /><small><a href="https://maps.google.de/maps?f=q&amp;source=embed&amp;hl=de&amp;geocode=&amp;q=Wendel+Schwimmbadbau,+Wilhelmstra%C3%9Fe,+Idar-Oberstein&amp;aq=0&amp;oq=wendel+schwimm&amp;sll=51.175806,10.454119&amp;sspn=9.289357,20.808105&amp;t=h&amp;ie=UTF8&amp;hq=Wendel+Schwimmbadbau,&amp;hnear=Wilhelmstra%C3%9Fe,+Oberstein+55743+Idar-Oberstein,+Rheinland-Pfalz&amp;ll=49.702513,7.335062&amp;spn=0.018707,0.040641&amp;z=14&amp;iwloc=A&amp;cid=27701883225859051" style="text-align:left">Größere Kartenansicht</a></small>
+                <iframe scrolling="no" src="https://maps.google.de/maps?f=q&amp;source=s_q&amp;hl=de&amp;geocode=&amp;q=Wendel+Schwimmbadbau,+Wilhelmstra%C3%9Fe,+Idar-Oberstein&amp;aq=0&amp;oq=wendel+schwimm&amp;sll=51.175806,10.454119&amp;sspn=9.289357,20.808105&amp;t=h&amp;ie=UTF8&amp;hq=Wendel+Schwimmbadbau,&amp;hnear=Wilhelmstra%C3%9Fe,+Oberstein+55743+Idar-Oberstein,+Rheinland-Pfalz&amp;ll=49.702513,7.335062&amp;spn=0.018707,0.040641&amp;z=14&amp;iwloc=A&amp;cid=27701883225859051&amp;output=embed"></iframe><br /><small><a href="https://maps.google.de/maps?f=q&amp;source=embed&amp;hl=de&amp;geocode=&amp;q=Wendel+Schwimmbadbau,+Wilhelmstra%C3%9Fe,+Idar-Oberstein&amp;aq=0&amp;oq=wendel+schwimm&amp;sll=51.175806,10.454119&amp;sspn=9.289357,20.808105&amp;t=h&amp;ie=UTF8&amp;hq=Wendel+Schwimmbadbau,&amp;hnear=Wilhelmstra%C3%9Fe,+Oberstein+55743+Idar-Oberstein,+Rheinland-Pfalz&amp;ll=49.702513,7.335062&amp;spn=0.018707,0.040641&amp;z=14&amp;iwloc=A&amp;cid=27701883225859051" style="text-align:left">Größere Kartenansicht</a></small>
             </div>
             <div class="col span6">
                 <h2>Anfahrt Mainz</h2>
-                <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.de/maps?f=q&amp;source=s_q&amp;hl=de&amp;geocode=&amp;q=Am+Schleifweg+15,+Mainz&amp;aq=0&amp;oq=am+schleifweg+15&amp;sll=49.704167,7.335048&amp;sspn=0.018707,0.040641&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Am+Schleifweg+15,+55128+Mainz,+Rheinland-Pfalz&amp;z=14&amp;ll=49.96801,8.24074&amp;output=embed"></iframe><br /><small><a href="https://maps.google.de/maps?f=q&amp;source=embed&amp;hl=de&amp;geocode=&amp;q=Am+Schleifweg+15,+Mainz&amp;aq=0&amp;oq=am+schleifweg+15&amp;sll=49.704167,7.335048&amp;sspn=0.018707,0.040641&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Am+Schleifweg+15,+55128+Mainz,+Rheinland-Pfalz&amp;z=14&amp;ll=49.96801,8.24074" style="text-align:left">Größere Kartenansicht</a></small>
+                <iframe src="https://maps.google.de/maps?f=q&amp;source=s_q&amp;hl=de&amp;geocode=&amp;q=Am+Schleifweg+15,+Mainz&amp;aq=0&amp;oq=am+schleifweg+15&amp;sll=49.704167,7.335048&amp;sspn=0.018707,0.040641&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Am+Schleifweg+15,+55128+Mainz,+Rheinland-Pfalz&amp;z=14&amp;ll=49.96801,8.24074&amp;output=embed"></iframe><br /><small><a href="https://maps.google.de/maps?f=q&amp;source=embed&amp;hl=de&amp;geocode=&amp;q=Am+Schleifweg+15,+Mainz&amp;aq=0&amp;oq=am+schleifweg+15&amp;sll=49.704167,7.335048&amp;sspn=0.018707,0.040641&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Am+Schleifweg+15,+55128+Mainz,+Rheinland-Pfalz&amp;z=14&amp;ll=49.96801,8.24074" style="text-align:left">Größere Kartenansicht</a></small>
             </div>
         </div>
 	</div>
