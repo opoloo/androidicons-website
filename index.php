0aab65134d192aa99a876941f1d5620dfab45d92
Sandra Scherer <sandra@opoloo.de>
2013-08-29 17:19:39 +0200

    Final Commit before Launch

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
         </div>
         <div class="set-info hide_mobile">
           <h2>You get what you need</h2>
-          <p>250 developer icons that work perfectly in your Android app, mobile interface, or website &ndash; in 5 sizes and 14 colors, including vector sources and a friendly license.</p>
+          <p>250 developer icons that work perfectly in your Android app, mobile interface, or website &ndash; in 5 sizes and 14 colors, including vector sources and a <a href="royalty_free_license.txt">friendly license</a>.</p>
           <div class="button-download"><a href="http://pul.ly/b/75286">
             250 icons, 5 sizes, 14 colors
            <span> Download <span>for</span> 25$</span>
@@ -301,7 +301,7 @@
         <div class="reviews hide_mobile">
           <div class="two-cols">
             <p>"These icons are perfectly crafted. It's an effortless way for developers to inject some visual polish into their apps."</p>
-            <span>&mdash; Jamie McDonald (Guardian)</span>
+            <span>&mdash; Jamie McDonald (Sudoku)</span>
           </div>
           <div class="two-cols">
             <p>"These are the only icons I want to see in my statusbar, and everybody should use them."</p>
