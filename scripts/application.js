0593a649bee53dd6076fed279530459727fd7512
Max Boll <max@opoloo.de>
2013-08-29 17:03:48 +0200

    Smooth scrolling

--- scripts/application.js
+++ scripts/application.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
   // Smooth Anchor Scrolling
   $('a').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+        scrollTop: $($(this).attr('href')).offset().top - 65
     }, 500);
     return false;
   });
