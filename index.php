baf0d62e4e2e05b194569c375e5b7da5a9e3a7aa
Sandra Scherer <sandra@opoloo.de>
2013-08-29 10:29:09 +0200

    Initial Commit for AI 2.5

--- index.php
+++ index.php
@@ -1,92 +1,245 @@
-<? $active = 'index' ?>
-<? require_once('includes/header.inc.php'); ?>
-<div id="header">
-    <div id="slideheader">
-        <ul class="slides" id="slider">
-            <li class="slide s1">
-                <a href="konzeption.php">
-                    Aquatische Lebensräume
-                    <span>Maßgefertigte Wasserlandschaften aus einer Hand </span>
-                </a>
-            </li>
-            <li class="slide s2">
-                <a href="inspiration.php#gartenbad">    
-                    Gartenbäder
-                    <span>Draußen. Zuhause.</span>
-                </a>
-            </li>
-            <li class="slide s3">
-                <a href="inspiration.php#hallenbad"> 
-                    Hallenbäder
-                    <span>Das Wohnzimmer, von dem Sie immer geträumt haben. Nur mit Wasser.</span>
-                </a>
-            </li>
-            <li class="slide s4">
-                <a href="inspiration.php#wellness"> 
-                    Wellness
-                   <span>Saunen, Thermen und Whirlpools. Individuell und vom Fachmann. </span>
-                </a>
-            </li>
-            <li class="slide s5">
-                <a href="inspiration.php#hotelbad"> 
-                    Hotelbäder
-                    <span>Für Ihre Gäste: nur das Beste. </span>
-                </a>
-            </li>
+<!doctype html>
+<html class="no-js">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="opoloo GbR" />
+<meta name="description" content="Android Icons offers free icons and a professional handcrafted icon set with 200 icons in 14 different colors and 4 sizes." />
+<meta name="keywords" content="android,icon,icons,icondesign,free,download,icon set,iconset, ics, jellybean, Honeycomb, mayence, new york, sources, royalty free" />
+<meta http-equiv="Content-Type"	content="text/html; charset=UTF-8" />
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,400italic,300italic,700|Roboto+Condensed:400,700,400italic,300italic,300|Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+
+<title>Android Icons | Developer Icons 2</title>
+
+<!-- Embedding Favicon -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="icon" href="favicon.png" type="image/png" />
+
+<!-- General Stylesheet -->
+<link href="stylesheets/output.css.scss" rel="stylesheet" type="text/css" />
+<link href="stylesheets/idangerous.swiper.css" rel="stylesheet" type="text/css" />
+
+<!-- Responsive -->
+<link href="stylesheets/media-queries.css" rel="stylesheet" type="text/css" />
+
+<!-- Fallback Stylesheet -->
+<link href="stylesheets/no-fontface.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript" src="scripts/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="scripts/idangerous.swiper.js"></script>
+<script type="text/javascript" src="scripts/application.js"></script>
+</head>
+
+
+<body>
+
+  <!-- Lightbox (Preview) -->
+  <div class="preview-box">
+    <div class="preview-panel">
+      <button class="btn-preview">Close preview</button>
+    </div>
+    <img src="images/preview-full.png" alt="" />
+  </div>
+
+  <!-- Begin Header -->
+  <header data-role="header">
+    <div class="wrapper">
+      <div id="logo">
+        <img src="images/logo.png" alt="Android Icons Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="">The Set</a></li>
+          <li><a href="">License</a></li>
         </ul>
+      </nav>
+      <a href="#" id="linktoset">
+        <span id="get-arrow">&darr;</span>Get the set 
+      </a>
+    </div>    
+  </header>
+  <!-- End Header -->
+
+  <div class="wrapper">
+    <div id="intro">
+      <h1>Android Developer Icons 2</h1>
+      <div id="teaser">
+        <p>250 handcrafted, pixelperfect icons in 5 sizes and 14 colors</p>
+        <span>optimized for Ice Cream Sandwich, Jelly Bean applications & the Web</span>
+      </div>
     </div>
-        <!-- <ul class="flex-direction-nav"><li><a class="flex-prev" href="#">Zurück</a></li><li><a class="flex-next" href="#">Weiter</a></li></ul> -->
-</div>
-<!-- End Header -->
-<!-- Begin Content -->
-<div class="bg-hell">
-    <div class="container">
-        <div class="row">
-            <div class="col span12">
-                <h1><span>//</span> Professionelle Schwimmbad-Architektur</h1>
-            </div>
+
+    <!-- Begin Content -->
+    <content data-role="content">
+      <div id="iconset">
+        <div class="set-preview">
+          <img src="images/androidicons_preview.png" alt="Android Icons Preview" />
+          <button class="btn-preview">Preview of all icons</button>
         </div>
-        <div class="row">
-            <div class="col span7">
-                <p class="teaser">Wir bauen Ihnen in Maßarbeit Ihr Wunsch-Schwimmbad.</p>
-                <p>Das sind bei uns nicht einfach nur Wasserbecken, sondern aquatische Wohnlandschaften, die wir mit Ihnen zusammen planen, konzipieren, designen und umsetzen.</p><br/>
-                <p>Mit hochentwickelter, effizienter, zukunftsweisender Technologie – umweltfreundlich und hygienisch. Wir betreuen und warten die Schwimmbäder von mehr als 4500 privaten und Hotelkunden.	</p>
-                <a href="konzeption.php" class="button-orange">Was können wir für Sie tun?</a>
-            </div>
-            <div class="col span5">
-                <img src="images/illu.png" alt="Illu" />
-            </div>
+        <div class="set-info">
+          <h2>You get what you need</h2>
+          <p>250 developer icons that work perfectly in your Android app, mobile interface, or website — 5 sizes and 14 colors, including vector sources and a friendly, royalty-free license.</p>
+          <div class="button-download"><a href="http://pul.ly/b/33404">Download for 25$</a></div>
         </div>
+        <div class="clearfix"></div>
+      </div>
+      <div id="quote">
+        <p>"Androidicons is an essential companion kit for any Android designer. I've used the kit in basically every recent design I've worked on. With such a friendly license and price, it's really a no-brainer."</p>
+        <span>—Roman Nurik (Google)</span>
+      </div>
+      <div class="clearfix"></div>     
+      <div id="spec">
+        <ul>
+          <li><h2>Enough is enough</h2><p>250 icons in 5 sizes and 14 beautiful colors? Holy barracuda Batman! That's 17,500 individual graphics. You probably won't need another icon set for Android development (or even web design) ever again. </p></li>
+          <li><h2>Apps & Interfaces</h2><p>Androidicons are lovingly designed to enhance your Android application or mobile interface. If you were looking for flat, full, pixelperfect, polished icons that precisely communicate your message, you have found them.</p></li>
+          <li><h2>For Developers & Designers</h2><p>Androidicons meticulously follow the Android 4.0 guidelines, so all icons will work perfectly in your Ice Cream Sandwich or Jelly Bean apps and the web, no matter if you use them in your status bar, for notifications, or to enhance your copy.</p></li>
+          <li><h2>Quality</h2><p>Every little pixel was carefully pushed into its position to create the ideal icon, in each size and color, designed and handcrafted by Guenther Beyer. We took great care to attend upon your needs and leave out all the useless stuff.</p></li>
+          <li><h2>Versatile</h2><p>You get 5 sizes with the set (hdpi, ldpi, mdpi, xhdpi & xxhdpi) and you can easily combine and remix individual icons, or create completely new works of art from the included source files, supporting AI, SVG & EPS. </p></li>
+          <li><h2>License</h2><p>Buy once, use forever. After you have purchased the set, you may use all icons in as many applications as you want, and there's no need for further royalties or credits. You can have a look at the license here.</p></li>
+        </ul>
+      </div>
+      <div class="clearfix"></div>  
+      <hr/>
+      
+      <div id="users">
+        <h1>Apps that use our icon set</h1>
+
+        <div class="logos">
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <!-- Slide 1 -->
+              <div class="swiper-slide">
+                <div class="logo">
+                  <img src="images/customers/dashclock.png" alt="" /><br />
+                  <span>Dashclock</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/cliffhanger.png" alt="" /><br />
+                  <span>Cliffhanger</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/runtastic.png" alt="" /><br />
+                  <span>Runtastic</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/mobdro.png" alt="" /><br />
+                  <span>Mobdro</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/tasker.png" alt="" /><br />
+                  <span>Tasker</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/sudoku.png" alt="" /><br />
+                  <span>Sudoku</span>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <!-- Slide 2 -->
+              <div class="swiper-slide">
+                <div class="logo">
+                  <img src="images/customers/clean-file-manager.png" alt="" /><br />
+                  <span>Clean File Manager</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/valet.png" alt="" /><br />
+                  <span>Valet</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/pushbullet.png" alt="" /><br />
+                  <span>Pushbullet</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/notif.png" alt="" /><br />
+                  <span>Notif</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/write.png" alt="" /><br />
+                  <span>Write</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/spotcommander.png" alt="" /><br />
+                  <span>Spotcommander</span>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+              <!-- Slide 3 -->
+              <div class="swiper-slide">
+                <div class="logo">
+                  <img src="images/customers/telly.png" alt="" /><br />
+                  <span>Telly</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/drinkinggames.png" alt="" /><br />
+                  <span>Drinkinggames</span>
+                </div>
+                <div class="logo">
+                  <img src="images/customers/money-tracker.png" alt="" /><br />
+                  <span>Money Tracker</span>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="reviews">
+          <div class="two-cols">
+            <p>"The set delivers great expressive ideas in a beautiful way. My users now see the meaning of a command in a small pretty picture."</p>
+            <span>—John Roberts (AddtoNYC)</span>
+          </div>
+          <div class="two-cols">
+            <p>"These are the only icons I want to see in my statusbar, and everybody should use them."</p>
+            <span>—Eric Carboni (Notif)</span>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+      <hr/>
+      <div id="freestuff">
+        <h1>Free Stuff</h1>
+        <div class="three-cols">
+          <h2>Test Icons</h2>
+          <a href="downloads/adi_bonus_2.1.zip" class="nostyle"><img src="images/bonus_01_preview.png" alt="Free Bonus Icons" class="picture" /></a>
+          <p>Get these 12 free icons in 5 sizes & 14 colors to see if they work for you. They're free.</p>
+        </div>
+         
+        <div class="three-cols">
+          <h2>Boiler Plates</h2>
+          <a href="downloads/adi_bonus_2.2.zip" class="nostyle"><img src="images/bonus_02_preview.png" alt="Free Bonus Icons" class="picture" /></a>
+          <p>If you're in need of a simple launchericon, just download these boilerplate icons and drag another Android-Icon on it — perfect fit. 12 colors and 5 sizes.</p>
+        </div>
+         
+        <div class="three-cols">
+          <h3>Wallpaper</h3>
+          <a href="downloads/boilerplates.zip" class="nostyle"><img src="images/bonus_03_preview.png" alt="Free Bonus Icons" class="picture" /></a>
+          <p>3 wallpapers with AndroidIcons, lovingly created for your homescreen. Just grab them.</p>
+        </div> 
+      </div>
+      <div class="clearfix"></div>
+    </content>
+    <!-- End Content -->
+
+  <!-- Begin Footer -->
+  <div id="footer">	
+    <div id="copyright">
+      <span class="about">The set was created by <a href="https://plus.google.com/u/0/109726284197282147930/posts">Guenther Beyer</a> and <a href="http://www.opoloo.com">Opoloo</a>. If you're looking for Android design and development services, feel free to <script type="text/javascript">eval(decodeURIComponent('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%5c%22%6d%61%69%6c%74%6f%3a%69%6e%66%6f%40%6f%70%6f%6c%6f%6f%2e%63%6f%6d%5c%22%3e%63%6f%6e%74%61%63%74%20%75%73%3c%5c%2f%61%3e%27%29%3b'))</script>.</span>
+      <span>All rights reserved &copy; 2013 <a href="http://www.opoloo.com">Opoloo GbR</a>. Lovingly created in Mayence | <a href="http://www.opoloo.de/impressum">Imprint</a></span>
+      <span class="signet"><a href="http://www.opoloo.com/"></a></span>
     </div>
+  </div>
 </div>
-<div class="bg-dunkel">
-    <div class="container">
-        <div class="row">
-            <div class="col span12">
-                <h1><span>//</span> Unsere Bäder</h1>
-                <p class="intro">Wir lieben Schwimmbäder, egal ob im Garten, als Wasserwohnzimmer oder im Hotel. Und wir finden garantiert das Schwimmbad, das zu Ihnen passt.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col span4">
-                <h2>Gartenbäder</h2>
-                <a href="inspiration.php#gartenbad"><img src="images/gartenbad_th.jpg" alt="Bad 1" class="border fade" /></a>
-            </div>
-            <div class="col span4">
-                <h2>Hallenbäder</h2>
-                <a href="inspiration.php#hallenbad"><img src="images/hallenbad_th.jpg" alt="Bad 2" class="border fade" /></a>
-            </div>
-            <div class="col span4">
-                <h2>Hotelbäder</h2>
-                <a href="inspiration.php#hotelbad"><img src="images/hotelbad_th.jpg" alt="Bad 3" class="border fade" /></a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col span12">
-                <a href="inspiration.php" class="button-orange">Lassen Sie sich inspirieren</a>
-            </div>
-        </div>
-	</div>
-</div>
-<!-- End Content -->
-<? require_once('includes/footer.inc.php'); ?>
+
+<script type="text/javascript">
+var pkBaseURL = "http://analytics.opoloo.de/"
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://analytics.opoloo.de/piwik.php?idsite=3" style="border:0" alt="" /></p></noscript>
+
+</body>
+</html>
--- scripts/application.js
+++ scripts/application.js
@@ -1,46 +1,31 @@
 $(document).ready(function(){
 
-    $("#slider").responsiveSlides({
-      auto: true,             // Boolean: Animate automatically, true or false
-      speed: 1500,            // Integer: Speed of the transition, in milliseconds
-      timeout: 6000,          // Integer: Time between slide transitions, in milliseconds
-      pager: true,           // Boolean: Show pager, true or false
-      nav: true,             // Boolean: Show navigation, true or false
-      random: false,          // Boolean: Randomize the order of the slides, true or false
-      pause: true,           // Boolean: Pause on hover, true or false
-      pauseControls: true,    // Boolean: Pause when hovering controls, true or false
-      prevText: "Zurück",   // String: Text for the "previous" button
-      nextText: "Weiter"       // String: Text for the "next" button
-    });
-
-	$('.gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tClose: 'Schlie&szlig;en',
-		tLoading: 'Lade Bild #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-   			tPrev: 'Vorheriges (Linke Pfeiltaste)',
-   			tNext: 'N&auml;chstes (Rechte Pfeiltaste)',
-   			tCounter: '%curr% / %total%',
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">Das Bild #%curr%</a> konnte nicht geladen werden.',
-			titleSrc: function(item) {
-				title = item.el.attr('title');
-				if (title == 'undefined') title = "";
-				return  title;
-			}
-		}
-	});
-
 	$('#totop a').on('click', function(e){
 		e.preventDefault();
 		$('html,body').animate({scrollTop: 0}, 1000);
         return false;
 	});
 
+  // Switch download arrow
+  $(document).scroll(function() {
+    if ($(document).scrollTop() > 650) {
+      $('#get-arrow').html("&uarr;");
+    } else {
+      $('#get-arrow').html("&darr;");
+    }
+  });
+
+  // Load slider
+  var mySwiper = $('.swiper-container').swiper({
+    //Your options here:
+    mode:'horizontal',
+    loop: true,
+    mousewheelControl: true
+  });
+
+  // Preview Set
+  $('.btn-preview').click(function() {
+    $('.preview-box, .wrapper').fadeToggle();
+    $('body').scrollTop(0);
+  });
 });
